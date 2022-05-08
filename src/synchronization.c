4458f86fb9a2daedac74eef8ae53ad13e93e4808
WaveProtector <74835208+WaveProtector@users.noreply.github.com>
2022-05-08 20:29:01 +0100

    Update synchronization.c
    Já podem usar esta função para fazer os semáforos nas vossas classes.

--- src/synchronization.c
+++ src/synchronization.c
@@ -1,37 +1,39 @@
 #include "synchronization.h"
-#include "string.h"
 #include "fcntl.h"
 
 sem_t * semaphore_create(char* name, int value) {
-    return sem_open(name, O_CREAT, 0666, value); //a professora não deu isto e diz para fazermos com getuid() mas não sei como criar semáforos sem usar o sem_open?
-                                                 //então é capaz de estar errado desta forma, apesar de funcionar.
+    return sem_open(name, O_CREAT, 0xFFFFFFFF, value);
 }
 
 void semaphore_destroy(char* name, sem_t* semaphore) {
     sem_close(semaphore);
-    sem_destroy(semaphore);
+    sem_unlink(name);
 }
 
 void produce_begin(struct prodcons* pc) {
-    //TODO
+    sem_wait(pc->empty);
+    sem_wait(pc->mutex);
 }
 
 void produce_end(struct prodcons* pc) {
-    //TODO
+    sem_post(pc->mutex);
+    sem_post(pc->full);
 }
 
 void consume_begin(struct prodcons* pc) {
-    //TODO
+    sem_wait(pc->full);
+    sem_wait(pc->mutex);
 }
 
 void consume_end(struct prodcons* pc) {
-    //TODO
+    sem_post(pc->mutex);
+    sem_post(pc->empty);
 }
 
 void semaphore_mutex_lock(sem_t* sem) {
-    //TODO
+    sem_wait(sem);
 }
 
 void semaphore_mutex_unlock(sem_t* sem) {
-    //TODO
+    sem_post(sem);
 }
