955cfd1a39b2640e1e8b70f84107ba2afa0c8a43
CodBestFish <jose.bras15@hotmail.com>
2022-05-19 19:34:18 +0100

    Mudanças ao mesignal. Espero estar a funcionar. Ficheiros estranhos foram criados

--- include/mesignal.h
+++ include/mesignal.h
@@ -4,4 +4,9 @@
 #include <signal.h>
 #include <unistd.h>
 
-void create_alarm(struct operation* op, struct config* config);
+//Imprime no terminal os dados de cada request feito
+void sig_handler();
+
+
+//Cria um alarme que dispara a cada config.alarm_time segundos.
+void create_alarm(struct main_data* data, struct config config);
--- src/mesignal.c
+++ src/mesignal.c
@@ -1,22 +1,41 @@
 #include "mesignal.h"
 
-void create_alarm(struct operation *op, struct config *config)
-{
-   signal(SIGALRM, create_alarm);
-   if(op->status == 'C') {
-   printf("request:%d status:C start:%d restaurant:%d rest_time:%d driver:%d driver_time:%d client:%d client_end_time:%d",
-          op->id,
-          op->start_time,
-          op->receiving_rest,
-          op->rest_time->tv_sec,
-          op->receiving_driver,
-          op->driver_time->tv_sec,
-          op->requesting_client,
-          op->client_end_time->tv_sec);
-   }
-   else {
-      printf("request:%d status:%c", op->id, op->status);
-   }
-   alarm(config->alarm_time);
+struct operation *op;
 
+void sig_handler()
+{
+   while (op != NULL)
+   {
+      if (op->status == 'C')
+      {
+         printf("request:%d status:C start:%ld restaurant:%d rest_time:%ld driver:%d driver_time:%ld client:%d client_end_time:%ld",
+                op->id,
+                op->start_time.tv_sec,
+                op->receiving_rest,
+                op->rest_time.tv_sec,
+                op->receiving_driver,
+                op->driver_time.tv_sec,
+                op->requesting_client,
+                op->client_end_time.tv_sec);
+      }
+      else
+      {
+         printf("request:%d status:%c", op->id, op->status);
+      }
+      op++;
+   }
+}
+
+void create_alarm(struct main_data *data, struct config config)
+{
+   op = data->results;
+   signal(SIGALRM, sig_handler);
+   int alarm_time = config.alarm_time;
+
+   while (1)
+   {
+      op = data->results;
+      alarm(alarm_time);
+      pause();
+   }
 }
